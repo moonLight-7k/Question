a804afb68a71bae3f978a41c51f2ed185d5bbd12
suhel khan <suhelkhan87514@gmail.com>
2023-01-24 13:44:15 +0530

    Added some code

---
+++ Conditional.c
@@ -0,0 +1,11 @@
+/*
+If 1<=n<=9, then print the lowercase English word corresponding to the number (e.g., one for 1, two for 2, etc.); otherwise, print Greater than 9 instead.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+}
