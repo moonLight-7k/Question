26fb198a40ec371f52b762f9af911077bc4bcf22
Suhel Khan <suhelkhan87514@gmail.com>
2023-05-03 21:46:53 +0530

    added more question

---
+++ Java/linkedListCycle.java
@@ -0,0 +1,20 @@
+public class linkedListCycle {
+    public int singleNumber(int[] nums) {
+
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            int count = 0;
+
+            for (int j = 0; j < len; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+
+            }
+            if (count == 1) {
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+}
