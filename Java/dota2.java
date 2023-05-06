ae4638f765fc636b7d23e4872a312ea5bbb47993
Suhel Khan <suhelkhan87514@gmail.com>
2023-05-06 22:46:10 +0530

    some fix

--- Java/dota2.java
+++ Java/dota2.java
@@ -4,10 +4,10 @@ import java.util.Queue;
 class Solution {
     public String predictPartyVictory(String senate) {
         Queue<Integer> qR = new LinkedList<>();
-        Queue<Integer> qD = new LinkedList<>();  
-        
+        Queue<Integer> qD = new LinkedList<>();
+
         int n = senate.length();
-        
+
         for (int i = 0; i < n; i++) {
             if (senate.charAt(i) == 'R') {
                 qR.offer(i);
@@ -15,17 +15,17 @@ class Solution {
                 qD.offer(i);
             }
         }
-        
+
         while (!qR.isEmpty() && !qD.isEmpty()) {
             int r = qR.poll();
             int d = qD.poll();
-            if (r < d) {  
+            if (r < d) {
                 qR.offer(r + n);
-            } else {  
+            } else {
                 qD.offer(d + n);
             }
         }
-        
+
         return qR.isEmpty() ? "Dire" : "Radiant";
     }
 }
