c3fb0aec405a17c5c4877038dc9cc40bcb625795
Suhel Khan <suhelkhan87514@gmail.com>
2023-05-08 14:20:25 +0530

    added code

--- Java/matrixDigonalSum.java
+++ Java/matrixDigonalSum.java
@@ -1,32 +1,15 @@
-import java.util.Scanner;
-
 class Solution {
-
-    public static void main(String[] args) {
-
-        Scanner scan = new Scanner(System.in);
-
-        int r = scan.nextInt();
-        int c = scan.nextInt();
-        int[][] mat = new int[r][c];
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                mat[i][j] = scan.nextInt();
-            }
-
-        }
+    public int diagonalSum(int[][] mat) {
         int sum = 0;
         int n = mat.length;
 
-        for (int k = 0; k < n; k++) {
-            if (k != n - k - 1) {
-                sum += mat[k][k] + mat[k][n - k - 1];
+        for (int i = 0; i < n; i++) {
+            if (i != n - i - 1) {
+                sum += mat[i][i] + mat[i][n - i - 1];
             } else {
-                sum += mat[k][k];
+                sum += mat[i][i];
             }
         }
-
+        return sum;
     }
-
 }
