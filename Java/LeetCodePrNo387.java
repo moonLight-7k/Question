4b574bfc8c9a182b688c56d49a4e756ef3fb1795
Suhel Khan <suhelkhan87514@gmail.com>
2023-05-12 21:31:45 +0530

    optimized the code

--- Java/LeetCodePrNo387.java
+++ Java/LeetCodePrNo387.java
@@ -1,25 +1,14 @@
 class Solution {
     public int firstUniqChar(String s) {
-
+        int[] count = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            int count = 0;
-            for (int j = 0; j < s.length(); j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (s.charAt(i) == s.charAt(j)) {
-                    count++;
-                    break;
-
-                }
-
-            }
-            if (count == 0) {
+            count[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1) {
                 return i;
             }
-
         }
         return -1;
-
     }
 }
