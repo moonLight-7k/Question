2908b30f8f37825be42153c96dd477db4cdbe404
Suhel Khan <suhelkhan87514@gmail.com>
2023-05-13 21:12:55 +0530

    Some fix

--- Java/PPS Java/bubbleSort.java
+++ Java/PPS Java/bubbleSort.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class bubbleSort {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter the size of the array: ");
         int size = input.nextInt();
         int[] arr = new int[size];
         System.out.print("Enter the elements of the array: ");
+        
         for (int i = 0; i < size; i++) {
             arr[i] = input.nextInt();
         }
